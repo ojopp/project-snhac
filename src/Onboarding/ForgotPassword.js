396babe2409f0f54b880dd79d18abe07a7ae1c46
Oscar Jopp <oscar.jopp29@gmail.com>
2017-12-05 11:54:39 +0000

    added Forgot password

--- src/Onboarding/ForgotPassword.js
+++ src/Onboarding/ForgotPassword.js
@@ -3,42 +3,54 @@ import {
   Text,
   View,
   StyleSheet,
-  Image,
 } from 'react-native';
+
+import OnbordingBackground from '../components/OnbordingBg';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    paddingTop: 20,
     position: 'absolute',
     width: '100%',
     height: '100%',
+    paddingTop: 20,
   },
   gradientBg: {
-    alignSelf: 'center',
-    flex: 1,
     width: '100%',
+    height: '100%',
   },
   txt: {
     color: '#232A30',
-    backgroundColor: '#00000000',
+    backgroundColor: 'transparent',
+  },
+  spacing: {
+    height: 20,
   },
 });
 
-const BackgroundSource = require('../assets/OnbordingBackground.png');
-
 export default class ForgotPassword extends React.Component {
-  static navigationOptions = {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+    };
   }
 
   render() {
     return (
       <View>
-        <Image stylr={styles.gradientBg} source={BackgroundSource} resizeMode="stretch" />
+        <OnbordingBackground />
         <View style={styles.container}>
-          <Text style={styles.txt}>
-            (Password reset link)
-          </Text>
+          <Input
+            placeholder="Email"
+            onChangeText={value => this.setState({ email: value })}
+            value={this.state.email}
+            keyboardType="email-address"
+          />
+          <View style={styles.spacing} />
+          <Button onPress={() => this.props.screenProps.forgotPassword(this.state.email)} text="Submit" />
         </View>
       </View>
     );
