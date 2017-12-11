c1a3e5d50609bfb8cece8f577366703a2ce25b3d
Oscar Jopp <oscar.jopp29@gmail.com>
2017-12-11 21:07:04 +0000

    added image to How To Athlete ID

--- src/Onboarding/HowToAthleteID.js
+++ src/Onboarding/HowToAthleteID.js
@@ -1,42 +1,32 @@
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-} from 'react-native';
+
 import styled from 'styled-components/native';
 
 import OnbordingBackground from '../components/OnbordingBg';
 import HowtoAthleteIDSource from '../assets/HowToAthleteID.png';
 
+const Container = styled.View`
+  flex: 1;
+`;
+
 const HowTo = styled.Image`
   width: 90%;
-  height: 60%;
+  height: 100%;
+  padding-bottom: 100
 `;
 
 const Space = styled.View`
   flex: 2;
 `;
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    paddingTop: 20,
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-  },
-  gradientBg: {
-    alignSelf: 'center',
-    flex: 1,
-    width: '100%',
-    height: '100%',
-  },
-  txt: {
-    color: '#232A30',
-    backgroundColor: '#00000000',
-  },
-});
+const InnerContainer = styled.View`
+  flex: 2;
+  align-items: center;
+  padding-bottom: 30%;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
 
 export default class HowToAthleteID extends React.Component {
   static navigationOptions = {
@@ -44,13 +34,13 @@ export default class HowToAthleteID extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <Container>
         <OnbordingBackground />
-        <View style={styles.container}>
+        <InnerContainer>
           <HowTo source={HowtoAthleteIDSource} resizeMode="contain" />
           <Space />
-        </View>
-      </View>
+        </InnerContainer>
+      </Container>
     );
   }
 }
