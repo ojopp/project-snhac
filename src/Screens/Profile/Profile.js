c026778d504cbcdd968a99653da34b35e36ce91c
Oscar Jopp <oscar.jopp29@gmail.com>
2017-12-19 12:05:29 +0000

    refactored profile using styled components rather than style sheet

--- src/Screens/Profile/Profile.js
+++ src/Screens/Profile/Profile.js
@@ -1,40 +1,97 @@
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import styled from 'styled-components/native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-  },
-  settings: {
-    paddingRight: 8,
-  },
-});
+const SettingsButton = styled.TouchableOpacity`
+  padding-right: 8px;
+`;
 
+const MainContainer = styled.View`
+  background-color: #ffffff;
+  flex:1;
+`;
+
+const SubContainer = styled.View`
+  height: 108px;
+  flex-direction: row;
+`;
+
+const RightContainer = styled.View`
+  flex:1
+`;
+
+const DPContainer = styled.View`
+  height: 108px;
+  width: 108px;
+  background-color: #00ff0090;
+  margin-left: 20px;
+`;
+
+const InfoContainer = styled.View`
+  flex-direction: row;
+`;
+
+const Info = styled.View`
+  height: 72px;
+  flex:1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InfoText = styled.Text`
+  color: #000000;
+  padding:5px;
+  font-weight: bold;
+`;
+
+const TabNavContainer = styled.View`
+  height: 36px;
+  flex:1;
+  background-color: #f0f0f0;
+`;
+
+const TabNavButton = styled.TouchableOpacity``;
 
 export default class ProfileScreen extends React.Component {
   static navigationOptions = ({ screenProps, navigation }) => ({
     headerRight: (
-      <TouchableOpacity
-        style={styles.settings}
-        // onPress={() => screenProps.signOut()}
+      <SettingsButton
         onPress={() => navigation.navigate('Settings')}
       >
-        <Icon name="settings" size={24} color='#232A30' />
-      </TouchableOpacity>
+        <Icon name="settings" size={24} color="#232A30" />
+      </SettingsButton>
     ),
   })
 
   render() {
     return (
-      <View style={styles.container} />
+      <MainContainer>
+        <SubContainer>
+          <DPContainer />
+          <RightContainer>
+            <InfoContainer>
+              <Info>
+                <InfoText>
+                  Age Group
+                </InfoText>
+                <InfoText>
+                  U20
+                </InfoText>
+              </Info>
+              <Info>
+                <InfoText>
+                  Lead Coach
+                </InfoText>
+                <InfoText>
+                  Norma Harris
+                </InfoText>
+              </Info>
+            </InfoContainer>
+            <TabNavContainer />
+          </RightContainer>
+        </SubContainer>
+      </MainContainer>
     );
   }
 }
