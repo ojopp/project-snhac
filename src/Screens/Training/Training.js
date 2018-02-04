847a8f6f310a888a1fc206b874250eb3e6566ed9
Oscar Jopp <oscar.jopp29@gmail.com>
2018-02-04 15:25:30 +0000

    started training page

--- src/Screens/Training/Training.js
+++ src/Screens/Training/Training.js
@@ -1,29 +1,51 @@
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-} from 'react-native';
+import styled from 'styled-components/native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import Schedule from '../../assets/Dec-Feb.png';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-  },
-});
+const MainContainer = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+`;
+
+const Training = styled.Image`
+  resize-mode: contain;
+  width: 100%;
+  height: 40%;
+`;
+
+const SectionHeader = styled.Text`
+  padding-vertical: 2;
+  padding-horizontal: 10;
+  font-size: 14;
+  font-weight: bold;
+  background-color: rgba(247, 247, 247, 1);
+`;
+
+const ImageButton = styled.TouchableOpacity``;
+
+const TrainingSchedule = styled.SectionList``;
 
 export default class TrainingScreen extends React.Component {
   static navigationOptions = {
-    tabBarIcon: ({ tintColor }) => (
-      <Icon name="fitness-center" size={34} color={tintColor} />
-    ),
-  }
+    tabBarIcon: ({ tintColor }) => <Icon name="fitness-center" size={34} color={tintColor} />,
+  };
 
   render() {
     return (
-      <View style={styles.container} />
+      <MainContainer>
+        <TrainingSchedule
+          sections={[{ title: '31 Dec - 8 Feb', data: [{ Schedule }] }]}
+          renderItem={({ item }) => (
+            <ImageButton underlayColor="#232A3030" onPress={() => navigate('EventDetailAthlete')}>
+              <Training source={item} />
+            </ImageButton>
+          )}
+          renderSectionHeader={({ section }) => <SectionHeader>{section.title}</SectionHeader>}
+          keyExtractor={(item, index) => index}
+        />
+      </MainContainer>
     );
   }
 }
