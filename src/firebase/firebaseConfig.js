ca8f706cc6ce5dec3adbc6855ef7fe60e64911c0
Oscar Jopp <oscar.jopp29@gmail.com>
2018-02-04 15:25:08 +0000

    prettied config

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAK0Dz0LZBHw1XmVMB7O7jbAhmA7GcD3MM",
-    authDomain: "project-snhac.firebaseapp.com",
-    databaseURL: "https://project-snhac.firebaseio.com",
-    projectId: "project-snhac",
-    storageBucket: "",
-    messagingSenderId: "54305322863"
-  };
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyAK0Dz0LZBHw1XmVMB7O7jbAhmA7GcD3MM',
+  authDomain: 'project-snhac.firebaseapp.com',
+  databaseURL: 'https://project-snhac.firebaseio.com',
+  projectId: 'project-snhac',
+  storageBucket: '',
+  messagingSenderId: '54305322863',
+};
 
 firebase.initializeApp(config);
 
